91909b6acba21b2879eeeb504a7934ac74b57f65
Jerônimo Barbieri Junior <jeronimo.barbieri@fatec.sp.gov.br>
2025-10-27 19:14:07 -0300

    feat: criada entidades ReportLocation, Comment, UseSavedReport

---
+++ CidadeIntegra/CidadeIntegra.Domain/Entities/Comment.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CidadeIntegra.Domain.Entities
+{
+    [Table("Comments")]
+    public class Comment
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [ForeignKey(nameof(Report))]
+        public Guid ReportId { get; set; }
+
+        [ForeignKey(nameof(Author))]
+        public Guid AuthorId { get; set; }
+
+        [Required, MaxLength(100)]
+        public string AuthorName { get; set; } = string.Empty;
+
+        [Required, MaxLength(50)]
+        public string AvatarColor { get; set; } = string.Empty;
+
+        [Required, MaxLength(500)]
+        public string Message { get; set; } = string.Empty;
+
+        [Required, MaxLength(50)]
+        public string Role { get; set; } = string.Empty;
+
+        public DateTimeOffset CreatedAt { get; set; }
+
+        // Navegações
+        public Report Report { get; set; } = null!;
+        public User Author { get; set; } = null!;
+    }
+}
---
+++ CidadeIntegra/CidadeIntegra.Domain/Entities/UserSavedReport.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CidadeIntegra.Domain.Entities
+{
+    [Table("UserSavedReports")]
+    public class UserSavedReport
+    {
+        [Key, Column(Order = 0)]
+        [ForeignKey(nameof(User))]
+        public Guid UserId { get; set; }
+
+        [Key, Column(Order = 1)]
+        [ForeignKey(nameof(Report))]
+        public Guid ReportId { get; set; }
+
+        public User User { get; set; } = null!;
+        public Report Report { get; set; } = null!;
+    }
+}
